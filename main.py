ff8c86d9a11f1457332dbb387bffb8741dd16843
Alex M <cmaldonado0@ucol.mx>
2018-09-29 18:28:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import email_lib
+from email_lib import email_lib
+
+def main():
+  From=str(raw_input("From: "))
+  Pass=str(raw_input("Password: "))
+  To=str(raw_input("To: "))
+  Subjet=str(raw_input("Subject: "))
+  Text=str(raw_input("Text: "))
+  con=email_lib(From,Pass,To,Subjet,Text)
+  con.send_email()
+if __name__=="__main__:
+  main()
